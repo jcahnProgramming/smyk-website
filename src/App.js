c701df37fb35a99654aa19f589bf92749a4408d8
Jamie Cahn <63986445+jcahnProgramming@users.noreply.github.com>
2023-05-01 12:33:47 -0600

    additions to package-lock, etc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route, Link, Outlet } from "react-router-dom";
 
 import NavBar from './components/NavBar';
