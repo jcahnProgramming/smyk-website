1964464c49e108164bc6ca7580fc2ef255a82093
Jenipher Belloso <jenipherbelloso@gmail.com>
2023-05-01 18:28:18 -0600

    router working

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React from "react";
 //import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route, Link, Outlet } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
 
-import NavBar from './components/NavBar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Joinus from './pages/Joinus';
-import Contact from './pages/Contact';
-import Shop from './pages/Shop';
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Joinus from "./pages/Joinus";
+import Contact from "./pages/Contact";
+import Shop from "./pages/Shop";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-
-      {/* <Router> */}
-            <NavBar />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/joinus' element={<Joinus />} />
-                <Route path='/contact' element={<Contact />} />
-                <Route path='/shop' element={<Shop />} />
-            </Routes>
-      {/* </Router> */}
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/joinUs" element={<Joinus />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
