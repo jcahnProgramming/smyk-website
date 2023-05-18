dab0078c25e3c336a15b0a5d8415430155a90117
Jamie Cahn <63986445+jcahnProgramming@users.noreply.github.com>
2023-05-18 17:43:17 -0600

    loginchip updated

--- src/components/Chips/LoginChip.js
+++ src/components/Chips/LoginChip.js
@@ -1,16 +1,18 @@
-import React from 'react';
-
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import './LoginChip.css';
 
 const LoginChip = (props) => {
+    
+    const navigate = useNavigate();
+
     function ChipHandler() {
-        <Link to="/useraccount" />
-    };
+        navigate('./useraccount');
+    }
+
 
   return (
-    <div className='chip' onClick={ChipHandler}>
+    <div className='chip' onClick={ChipHandler} style={{cursor: 'pointer'}}>
         <div>
             <img className='chip__img' src={require('../../img/logo.png')} />
         </div>
